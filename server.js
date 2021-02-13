4a1b603e06d5271821708889793c3d6964673048
ashishmusani <ashish_musani@ymail.com>
2021-02-13 17:10:46 +0530

    test_9

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, './client/build')));
 
-const dbConnectionUrl = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dgglj.mongodb.net/instant_messenger?retryWrites=true&w=majority`
+const dbConnectionUrl = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dgglj.mongodb.net/symposium?retryWrites=true&w=majority`
 mongoose.connect(dbConnectionUrl, {'useNewUrlParser': true, useUnifiedTopology: true } )
         .catch(err => {
           console.log(err);
