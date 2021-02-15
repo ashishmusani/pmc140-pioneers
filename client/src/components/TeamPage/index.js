ab44c4be3242c279ec6a8f761cfaab955f6ae0ba
ashishmusani <ashish_musani@ymail.com>
2021-02-15 20:39:44 +0530

    V2_Release8

--- client/src/components/TeamPage/index.js
+++ client/src/components/TeamPage/index.js
@@ -16,6 +16,11 @@ const TeamPage = () => {
                         <div className="team-card-body">
                             Ashish Musani 
                         </div>
+                        <div className="team-linkedin">
+                            <a href="https://www.linkedin.com/in/ashishmusani/" target="_blank">
+                                <img className="team-linkedin-photo" src="linkedin.png" />
+                            </a>
+                        </div>
                     </Card>
                 </Col>
                 <Col sm="3" md="3" xs="6">
@@ -23,6 +28,11 @@ const TeamPage = () => {
                         <Card.Img className="team-card-image" variant="top" src="sonali.jpg" />
                         <div className="team-card-body">
                             Sonali S.
+                        </div>
+                        <div className="team-linkedin">
+                            <a href="http://linkedin.com/in/sonali-saravanakumar-52a569174" target="_blank">
+                                <img className="team-linkedin-photo" src="linkedin.png" />
+                            </a>
                         </div>
                     </Card>
                 </Col>
@@ -32,6 +42,11 @@ const TeamPage = () => {
                         <div className="team-card-body">
                             Neethi Geever 
                         </div>
+                        <div className="team-linkedin">
+                            <a href="https://www.linkedin.com/in/neethi-geever-a4126b82/" target="_blank">
+                                <img className="team-linkedin-photo" src="linkedin.png" />
+                            </a>
+                        </div>
                     </Card>
                 </Col>
                 <Col sm="3" md="3" xs="6">
@@ -39,6 +54,11 @@ const TeamPage = () => {
                         <Card.Img className="team-card-image" variant="top" src="andrei.jpg" />
                         <div className="team-card-body">
                             Andrei Tarakanov
+                        </div>
+                        <div className="team-linkedin">
+                            <a href="https://www.linkedin.com/in/avbuka/" target="_blank">
+                                <img className="team-linkedin-photo" src="linkedin.png" />
+                            </a>
                         </div>
                     </Card>
                 </Col>
