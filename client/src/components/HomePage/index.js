495459de535bd4310ade3aa6c93bde2e0a5df0a0
ashishmusani <ashish_musani@ymail.com>
2021-03-03 20:57:06 +0530

    v3_release2

--- client/src/components/HomePage/index.js
+++ client/src/components/HomePage/index.js
@@ -3,23 +3,6 @@ import './HomePage.css';
 
 
 const HomePage = () => {
-
-    // const scheduleDateTime = new Date(Date.UTC(2021,2,18,2,0))
-
-    // const date = scheduleDateTime.getDate();
-    // const tz = Intl.DateTimeFormat().resolvedOptions().timeZone;
-
-    // function formatAMPM(date) {
-    //     var hours = date.getHours();
-    //     var minutes = date.getMinutes();
-    //     var ampm = hours >= 12 ? 'pm' : 'am';
-    //     hours = hours % 12;
-    //     hours = hours ? hours : 12; // the hour '0' should be '12'
-    //     minutes = minutes < 10 ? '0'+minutes : minutes;
-    //     var strTime = hours + ':' + minutes + ' ' + ampm;
-    //     return strTime;
-    // }
-
     return (
         <div className="homepage" >
             <div className="homepage-header">Pioneers</div>
@@ -33,11 +16,14 @@ const HomePage = () => {
                 <i>Taking teams back in time to leap forward</i>
             </div>
             <div className="homepage-schedule">
-                17 Feb, 2021 9:00 PM EST<br/>
-                18 Feb, 2021 7:30 AM IST
+                3 March, 2021 9:00 PM EST<br/>
+                4 March, 2021 7:30 AM IST
             </div>
             <div className="homepage-info">
-                Zoom meeting link will be available here before the session
+                <a href="https://senecacollege-ca.zoom.us/j/95951046144?pwd=NW5TTVpYbWdHbWI1YklZUmQvbVBaZz09"
+                target="_blank">
+                    <img className="homepage-zoom_logo" src="zoom_logo.png"/>
+                </a>
             </div>
         </div>        
     )
